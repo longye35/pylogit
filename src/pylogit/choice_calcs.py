78bf29bd2774f025dbb356349d645c84f7c7c6e2
longye35 <ryuyakyo25@gmail.com>
2021-09-10 08:47:21 +0900

    Fix: modify the weight calculation

--- src/pylogit/choice_calcs.py
+++ src/pylogit/choice_calcs.py
@@ -929,8 +929,9 @@ def calc_fisher_info_matrix(beta,
         weights = np.ones(design.shape[0])
     elif set(weights) == set(np.ones(design.shape[0])):
         weights_per_obs = np.ones(rows_to_obs.get_shape()[1])
-    weights_per_obs =\
-        np.max(rows_to_obs.toarray() * weights[:, None], axis=0)
+    else:
+        weights_per_obs =\
+            np.max(rows_to_obs.toarray() * weights[:, None], axis=0)
 
     ##########
     # Get the required matrices
